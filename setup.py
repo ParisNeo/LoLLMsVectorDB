3a464525c80db27a453338e1399cc58748f2aa96
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-07-30 02:29:09 +0200

    new v

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
     name='lollmsvectordb',
-    version='0.7.7',
+    version='0.7.8',
     description='A modular text-based database manager for retrieval-augmented generation (RAG), seamlessly integrating with the LoLLMs ecosystem.',
     long_description=long_description,
     long_description_content_type='text/markdown',
